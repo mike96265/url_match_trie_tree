58f5af9361b4236799439dd6014d6b04546d1531
yukityan <luvjoey1996@gmail.com>
2019-07-15 14:55:12 +0800

    Create trigx_tree.h

---
+++ trigx_tree.h
@@ -0,0 +1,26 @@
+//
+// Created by luvjo on 2019/7/13.
+//
+
+#include <pcre.h>
+
+#ifndef TRIGX_TREE_TRIGX_TREE_H
+#define TRIGX_TREE_TRIGX_TREE_H
+typedef struct _TrigxNode {
+    int val;
+    struct _TrigxNode *char_nodes[64];
+    struct _TrigxNode *rgx_next;
+    int rgx_raw_len;
+    pcre *re;
+    char *rgx;
+} TrigxNode;
+
+TrigxNode *create_trigx_node();
+
+void trigx_insert(TrigxNode *root, const char *word, int len_word, int val);
+
+int trigx_search(TrigxNode *root, const char *word, int word_len);
+
+void trigx_free(TrigxNode *root);
+
+#endif //TRIGX_TREE_TRIGX_TREE_H
