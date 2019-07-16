725aa2937776f882d5732b536e66bf044d7bba1a
yukityan <luvjoey1996@gmail.com>
2019-07-17 00:42:57 +0800

    Update trigx_tree.h

--- trigx_tree.h
+++ trigx_tree.h
@@ -11,7 +11,6 @@ typedef struct _TrigxNode {
     struct _TrigxNode *char_nodes[64];
     struct _TrigxNode *rgx_next;
     int rgx_raw_len;
-    pcre *re;
     char *rgx;
 } TrigxNode;
 
@@ -19,8 +18,9 @@ TrigxNode *create_trigx_node();
 
 void trigx_insert(TrigxNode *root, const char *word, int len_word, int val);
 
-int trigx_search(TrigxNode *root, const char *word, int word_len);
+int trigx_search(TrigxNode *root, const char *word, int len_word);
 
 void trigx_free(TrigxNode *root);
+
 
 #endif //TRIGX_TREE_TRIGX_TREE_H
